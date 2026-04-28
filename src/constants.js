326917a7852b2f3beaec2b4525f113ed08e45fe8
Daksh121004 <dakshpandit8076@gmail.com>
2026-04-28 22:10:31 +0530

    Update constants: classSection and semester data

--- src/constants.js
+++ src/constants.js
@@ -23,9 +23,15 @@ export const RATING_QUESTIONS = [
 
 
 
-export const semester = [1,2,3,4,5,6,7,8,9,10];
+export const semester = [2, 4, 6, 8, 10];
 
 export const classSection = {
-  "Computer Science & Technology": ["CSE 4A", "CSE 4B", "CSE 4C"],
-  "Mechanical Enginnering": ["ME 4A", "ME 4B", "ME 4C"]
+  "Computer Science & Technology": ["CSE 2A", "CSE 2B", "CSE 2C", "AIML 2A", "AIML 2B", "AIML 2C + FSD 2", "CSTI 2 + GENAI 2", "CSE 4A", "CSE 4B", "CSE 4C", "CSE 4D", "AIML 4A", "AIML 4B", "CSTI 4 + FSD4", "CSE 6A", "CSE 6B", "CSE 6C", "AIML 6A", "AIML 6B", "AIML 6C", "FSD 6", "CSTI6"],
+  "Mechanical Enginnering": ["ME 2", "ME 4", "ME 6", "M.Tech ME 2", "M.Tech ME 4"],
+  "Law": ["BA LLB II", "BBALLB II", "LLB II", "LLM II", "BBA LLB IV", "BA LLB IV", "LLB IV", "BBA LLB VI", "BA LLB VI", "LLB VI", "BBA LLB VIII", "BA LLB VIII", "BBA LLB X", "BA LLB X"],
+  "Bussiness": ["BBA BA II", "BBA EFB II", "BBA FAA II", "BBA GOM II", "MBA II", "BBA BA IV", "BBA EFB IV", "BBA FAA IV", "BBA GOM IV", "MBA IV", "BBA BA IV", "BBA EFB VI", "BBA FAA VI", "BBA GOM VI", "BBA GHCM VI"],
+  "Robotics & Artificial Intelligence": ["R&AI 2", "R&AI 4", "R&AI 6"],
+  "Electronics & Communication": ["ECE 2", "ECE 4", "ECE 6"],
+  "Sciences": ["M.Sc.(Chemistry) II", "M.Sc. (Chemistry) IV", "M.Sc.(Physics) IV", "M.Sc.(Maths) IV"],
+  "Education and Humanities": ["B.Ed Sem 2", "B Sc B.Ed Sem 2", "B.A B.Ed Sem 2", "B Sc B.Ed Sem 4", "B.A B.Ed Sem 4", "B Sc B.Ed Sem 6", "B.A B.Ed Sem 6", "B Sc B.Ed Sem 8", "B.A B.Ed Sem 8"]
 };
